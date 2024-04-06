69496f8fd835ff35b60e0416f6ce1b9034b5f0b0
Toxic2wild <166159890+Toxic2wild@users.noreply.github.com>
2024-04-06 08:33:59 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['08103960590']
+global.ownername = "💙𓆩‎✘Toxic𓆪_✰"//owner name
+global.ytname = "YT: toxicBotz.inc"
+global.socialm = "GitHub: toxicpandey023"
+global.location = "Nigerian i"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Toxic-𝐌𝐃' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
@@ -16,7 +16,7 @@ global.packname = 'Sticker By'
 global.author = 'Maria Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "toxic botz inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
